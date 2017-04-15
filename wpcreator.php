a0536e5df97cf568f11235ee8928ba4747d7b543
Thiago Loureiro <escazi1@gmail.com>
2017-04-15 07:16:01 -0400

    second commit

---
+++ wpcreator.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: WPCreator
+Plugin URI:  https://example.org
+Description: This plugin creates a new Wordpress instances under the root Wordpress instance using wp-cli
+Version:     20170415
+Author:      Thiago Loureiro
+Author URI:  https://tloureiro.com/
+License:     WTFPL
+Text Domain: wpcreator
+Domain Path: /languages
+*/
+
